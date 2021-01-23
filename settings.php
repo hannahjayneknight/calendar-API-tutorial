a40d91612756eae9604fc3948c494957ba02af7d
Hannah Jayne Knight <hannah@tinpot.co.uk>
2021-01-23 12:05:48 +0000

    gitignore

---
+++ settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Google App Client Id */
+define('CLIENT_ID', '788016050-vmjmtgkts82opbkcunl8ki6kha03931q.apps.googleusercontent.com');
+
+/* Google App Client Secret */
+define('CLIENT_SECRET', '6B1K2Q6yX1AW_aRjTRioCk0G');
+
+/* Google App Redirect Url */
+define('CLIENT_REDIRECT_URL', 'http://localhost:8080/google-login.php');
+
+?>
